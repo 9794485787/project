9c40d4aa34cac164bf0497d34b11a37eea54f0d8
Nehal Khan <nehalkhan4639@gmail.com>
2024-04-15 11:25:06 +0530

    second changes

--- dbConnect.php
+++ dbConnect.php
@@ -1,6 +1,6 @@
 <?php  
 // Connect with the database  
-$db = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD1, DB_NAME);  
+$db = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);  
   
 // Display error if failed to connect  
 if ($db->connect_errno) {  
